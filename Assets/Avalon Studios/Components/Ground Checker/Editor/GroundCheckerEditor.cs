b07adc5a0bbeca0bce984067ebc1b819baabf353
Braian Alejandro Ledesma <braian.ledesma-99@hotmail.com>
2020-09-15 09:14:48 -0300

    Add GroundChecker component to AvalonStudios package

---
+++ Assets/Avalon Studios/Components/Ground Checker/Editor/GroundCheckerEditor.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace AvalonStudios.Additions.Components.GroundCheckers
+{
+    [CustomEditor(typeof(GroundChecker))]
+    public class GroundCheckerEditor : Editor
+    {
+        private GroundChecker groundChecker;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUI.BeginChangeCheck();
+
+            if (GUILayout.Button("Reset") && groundChecker.GroundCheckPoint.Count != 0)
+            {
+                Undo.RecordObject(groundChecker, "Reset point");
+                groundChecker.ResetPoint();
+            }
+
+            if (EditorGUI.EndChangeCheck())
+                SceneView.RepaintAll();
+        }
+
+        private void OnSceneGUI() => Draw();
+
+        private void Draw()
+        {
+            Vector3[] points = groundChecker.GroundCheckPoint.ToArray();
+            foreach (Vector3 p in points)
+            {
+                Vector3 newPoint = Handles.PositionHandle(p, Quaternion.identity);
+
+                if (p != newPoint)
+                {
+                    Undo.RecordObject(groundChecker, "Move point");
+                    groundChecker.MovePoint(System.Array.IndexOf(points, p), newPoint);
+                }
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (groundChecker == null)
+                groundChecker = target as GroundChecker;
+
+            if (groundChecker.GroundCheckPoint.Count == 0)
+                groundChecker.Initialize();
+        }
+    }
+}
---
+++ Assets/Avalon Studios/Components/Ground Checker/GroundChecker.cs
@@ -0,0 +1,68 @@
+﻿using AvalonStudios.Additions.Extensions;
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace AvalonStudios.Additions.Components.GroundCheckers
+{
+    public class GroundChecker : MonoBehaviour
+    {
+        public List<Vector3> GroundCheckPoint
+        {
+            get
+            {
+                return groundCheckerPoint;
+            }
+            set
+            {
+                groundCheckerPoint = value;
+            }
+        }
+
+        [SerializeField, Tooltip("Point to check.")]
+        private List<Vector3> groundCheckerPoint = new List<Vector3>();
+
+        [SerializeField, Tooltip("Ground object radius.")]
+        private float radius = .4f;
+
+        [SerializeField, Tooltip("Layer mask.")]
+        private LayerMask layersToDetect = 0;
+
+        public void Initialize()
+        {
+            groundCheckerPoint = new List<Vector3>
+            {
+                transform.position + Vector3.down.MultiplyingVectorByANumber(1)
+            };
+        }
+
+        private void LateUpdate()
+        {
+            FollowParentObject();
+        }
+
+        public void ResetPoint() => Initialize();
+
+        public void MovePoint(int i, Vector3 p) => groundCheckerPoint[i] = p;
+
+        public void FollowParentObject()
+        {
+            Vector3[] points = groundCheckerPoint.ToArray();
+            foreach (Vector3 p in points)
+            {
+                Vector3 newPos = Vector3.Lerp(p, transform.position, 10f);
+                MovePoint(System.Array.IndexOf(points, p), newPos);
+            }
+        }
+
+        public bool IsGrounded() => Physics.CheckSphere(groundCheckerPoint.First(), radius, layersToDetect);
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            if (groundCheckerPoint.Count != 0)
+                Gizmos.DrawWireSphere(groundCheckerPoint.First(), radius);
+        }
+    }
+}
