34cd54a7b7cfb4eb08791eb664a9baae11d8ea45
Enderlook <enderlook@gmail.com>
2020-09-16 11:03:35 -0300

    Extract PlayerBrain from ThirdPersonMovementRigidbody

---
+++ Assets/Scripts/Player/PlayerBrain.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+
+namespace Avoidance.Player
+{
+    [RequireComponent(typeof(ThirdPersonMovementRigidbody)), DefaultExecutionOrder(3)]
+    public class PlayerBrain : MonoBehaviour
+    {
+        private enum PlayerState
+        {
+            Idle,
+            Sneaking,
+            Walking,
+            Running,
+        }
+
+        private enum PlayerEvent
+        {
+            StartSneaking,
+            StartWalking,
+            StartRunning,
+            StopMovement,
+            StopSneaking,
+            StopRunning,
+        }
+
+        private StateMachine<PlayerState, PlayerEvent> stateMachine;
+
+        private ThirdPersonMovementRigidbody movementSystem;
+
+        private void Awake()
+        {
+            movementSystem = GetComponent<ThirdPersonMovementRigidbody>();
+
+            stateMachine = StateMachine<PlayerState, PlayerEvent>.Builder()
+                .SetInitialState(PlayerState.Idle)
+                .In(PlayerState.Idle)
+                    .ExecuteOnEntry(movementSystem.SetIdle)
+                    .On(PlayerEvent.StartSneaking)
+                        .Goto(PlayerState.Sneaking)
+                    .On(PlayerEvent.StartWalking)
+                        .Goto(PlayerState.Walking)
+                    .On(PlayerEvent.StartRunning)
+                        .Goto(PlayerState.Running)
+                    .Ignore(PlayerEvent.StopRunning)
+                    .Ignore(PlayerEvent.StopSneaking)
+                .In(PlayerState.Sneaking)
+                    .ExecuteOnEntry(movementSystem.SetSneaking)
+                    .On(PlayerEvent.StopMovement)
+                        .Goto(PlayerState.Idle)
+                    .On(PlayerEvent.StartRunning)
+                        .Goto(PlayerState.Running)
+                    .On(PlayerEvent.StopSneaking)
+                        .Goto(PlayerState.Walking)
+                    .Ignore(PlayerEvent.StopRunning)
+                    .Ignore(PlayerEvent.StartWalking)
+                .In(PlayerState.Walking)
+                    .ExecuteOnEntry(movementSystem.SetWalking)
+                    .On(PlayerEvent.StopMovement)
+                        .Goto(PlayerState.Idle)
+                    .On(PlayerEvent.StartSneaking)
+                        .Goto(PlayerState.Sneaking)
+                    .On(PlayerEvent.StartRunning)
+                        .Goto(PlayerState.Running)
+                    .Ignore(PlayerEvent.StopRunning)
+                    .Ignore(PlayerEvent.StopSneaking)
+                .In(PlayerState.Running)
+                    .ExecuteOnEntry(movementSystem.SetRunning)
+                    .On(PlayerEvent.StopMovement)
+                        .Goto(PlayerState.Idle)
+                    .On(PlayerEvent.StartSneaking)
+                        .Goto(PlayerState.Sneaking)
+                    .On(PlayerEvent.StopRunning)
+                        .Goto(PlayerState.Walking)
+                    .Ignore(PlayerEvent.StopSneaking)
+                    .Ignore(PlayerEvent.StartWalking)
+                .Build();
+            stateMachine.Start();
+        }
+
+        private void Update()
+        {
+            switch (stateMachine.State)
+            {
+                case PlayerState.Sneaking:
+                    if (!movementSystem.IsSneaking)
+                        stateMachine.Fire(PlayerEvent.StopSneaking);
+                    if (!movementSystem.IsMoving)
+                        stateMachine.Fire(PlayerEvent.StopMovement);
+                    break;
+                case PlayerState.Running:
+                    if (!movementSystem.IsRunning)
+                        stateMachine.Fire(PlayerEvent.StopRunning);
+                    if (!movementSystem.IsMoving)
+                        stateMachine.Fire(PlayerEvent.StopMovement);
+                    break;
+                case PlayerState.Idle:
+                    if (movementSystem.IsMoving)
+                        stateMachine.Fire(PlayerEvent.StartWalking);
+                    break;
+                case PlayerState.Walking:
+                    if (movementSystem.IsSneaking)
+                        stateMachine.Fire(PlayerEvent.StartSneaking);
+                    else if (movementSystem.IsRunning)
+                        stateMachine.Fire(PlayerEvent.StartRunning);
+                    if (!movementSystem.IsMoving)
+                        stateMachine.Fire(PlayerEvent.StopMovement);
+                    break;
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/ThirdPersonMovementRigidbody.cs
+++ Assets/Scripts/Player/ThirdPersonMovementRigidbody.cs
@@ -2,33 +2,13 @@
 using AvalonStudios.Additions.Components.GroundCheckers;
 using AvalonStudios.Additions.Utils.InputsManager;
 
-using System;
-
 using UnityEngine;
 
 namespace Avoidance.Player
 {
-    [RequireComponent(typeof(Rigidbody)), DefaultExecutionOrder(1)]
+    [RequireComponent(typeof(Rigidbody)), DefaultExecutionOrder(2)]
     public class ThirdPersonMovementRigidbody : MonoBehaviour
     {
-        private enum PlayerState
-        {
-            Idle,
-            Sneaking,
-            Walking,
-            Running,
-        }
-
-        private enum PlayerEvent
-        {
-            StartSneaking,
-            StartWalking,
-            StartRunning,
-            StopMovement,
-            StopSneaking,
-            StopRunning,
-        }
-
 #pragma warning disable CS0649
         [Header("Inputs")]
         [SerializeField]
@@ -61,7 +41,6 @@ namespace Avoidance.Player
         private FreeLookCamera freeLook;
         private Vector3 movement;
         private Transform mainCamera;
-        private StateMachine<PlayerState, PlayerEvent> stateMachine;
 
         private float targetAngle;
         private float turnSmoothVelocity;
@@ -80,120 +59,50 @@ namespace Avoidance.Player
                 if (pivot != null)
                     freeLook = pivot.parent.GetComponent<FreeLookCamera>();
             }
-
-            stateMachine = StateMachine<PlayerState, PlayerEvent>.Builder()
-                .SetInitialState(PlayerState.Idle)
-                .In(PlayerState.Idle)
-                    .ExecuteOnEntry(SetMovement(0, 0))
-                    .On(PlayerEvent.StartSneaking)
-                        .Goto(PlayerState.Sneaking)
-                    .On(PlayerEvent.StartWalking)
-                        .Goto(PlayerState.Walking)
-                    .On(PlayerEvent.StartRunning)
-                        .Goto(PlayerState.Running)
-                    .Ignore(PlayerEvent.StopRunning)
-                    .Ignore(PlayerEvent.StopSneaking)
-                .In(PlayerState.Sneaking)
-                    .ExecuteOnEntry(SetMovement(sneakingSpeed, .34f))
-                    .On(PlayerEvent.StopMovement)
-                        .Goto(PlayerState.Idle)
-                    .On(PlayerEvent.StartRunning)
-                        .Goto(PlayerState.Running)
-                    .On(PlayerEvent.StopSneaking)
-                        .Goto(PlayerState.Walking)
-                    .Ignore(PlayerEvent.StopRunning)
-                    .Ignore(PlayerEvent.StartWalking)
-                .In(PlayerState.Walking)
-                    .ExecuteOnEntry(SetMovement(walkingSpeed, .84f))
-                    .On(PlayerEvent.StopMovement)
-                        .Goto(PlayerState.Idle)
-                    .On(PlayerEvent.StartSneaking)
-                        .Goto(PlayerState.Sneaking)
-                    .On(PlayerEvent.StartRunning)
-                        .Goto(PlayerState.Running)
-                    .Ignore(PlayerEvent.StopRunning)
-                    .Ignore(PlayerEvent.StopSneaking)
-                .In(PlayerState.Running)
-                    .ExecuteOnEntry(SetMovement(runningSpeed, 1))
-                    .On(PlayerEvent.StopMovement)
-                        .Goto(PlayerState.Idle)
-                    .On(PlayerEvent.StartSneaking)
-                        .Goto(PlayerState.Sneaking)
-                    .On(PlayerEvent.StopRunning)
-                        .Goto(PlayerState.Walking)
-                    .Ignore(PlayerEvent.StopSneaking)
-                    .Ignore(PlayerEvent.StartWalking)
-                .Build();
-            stateMachine.Start();
-
-            Action SetMovement(float speed, float animation)
-            {
-                return () =>
-                {
-                    movementSpeed = speed;
-                    playerAnimation.PlayLocomotion(animation);
-                };
-            }
         }
 
-        private void Update()
+        private void SetMovement(float speed, float animation)
         {
-            switch (stateMachine.State)
-            {
-                case PlayerState.Sneaking:
-                    if (!sneakInput.Execute())
-                        stateMachine.Fire(PlayerEvent.StopSneaking);
-                    break;
-                case PlayerState.Running:
-                    if (!runInput.Execute())
-                        stateMachine.Fire(PlayerEvent.StopRunning);
-                    break;
-                case PlayerState.Idle:
-                    if (Input.GetAxis(horizontalInput) != 0 || Input.GetAxis(verticalInput) != 0)
-                        stateMachine.Fire(PlayerEvent.StartWalking);
-                    break;
-                case PlayerState.Walking:
-                    if (sneakInput.Execute())
-                        stateMachine.Fire(PlayerEvent.StartSneaking);
-                    else if (runInput.Execute())
-                        stateMachine.Fire(PlayerEvent.StartRunning);
-                    break;
-            }
+            movementSpeed = speed;
+            playerAnimation.PlayLocomotion(animation);
         }
+
+        public void SetSneaking() => SetMovement(sneakingSpeed, .34f);
+
+        public void SetWalking() => SetMovement(walkingSpeed, .84f);
+
+        public void SetRunning() => SetMovement(runningSpeed, 1);
+
+        public void SetIdle() => SetMovement(0, 0);
+
+        public bool IsSneaking => sneakInput.Execute();
+
+        public bool IsRunning => runInput.Execute();
+
+        public bool IsMoving => movement != Vector3.zero;
 
         private void FixedUpdate()
         {
             if (!groundChecker.IsGrounded())
                 return;
 
-            switch (stateMachine.State)
-            {
-                case PlayerState.Sneaking:
-                case PlayerState.Walking:
-                case PlayerState.Running:
-                    Move();
-                    break;
-            }
+            movement.Set(Input.GetAxis(horizontalInput), 0, Input.GetAxis(verticalInput));
+
+            if (movementSpeed > 0)
+                Move();
+
             Rotate(movement);
         }
 
         private void Move()
         {
-            movement.Set(Input.GetAxis(horizontalInput), 0, Input.GetAxis(verticalInput));
-            if (movement == Vector3.zero)
-                stateMachine.Fire(PlayerEvent.StopMovement);
-            else
-            {
-                Vector3 dir = movement;
-                dir.y = 0;
-                if (dir.normalized != Vector3.zero)
-                {
-                    Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-                    Vector3 newMovement = moveDir.normalized * Time.fixedDeltaTime * movementSpeed;
-                    rigidbody.MovePosition(rigidbody.position + newMovement);
-                }
-            }
+            Vector3 dir = movement;
+            dir.y = 0;
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            Vector3 newMovement = moveDir.normalized * Time.fixedDeltaTime * movementSpeed;
+            rigidbody.MovePosition(rigidbody.position + newMovement);
         }
+
 
         private void Rotate(Vector3 move)
         {
