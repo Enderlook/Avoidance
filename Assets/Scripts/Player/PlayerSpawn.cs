71af40bf8b97040d2c7fb49dca35b81fa6d8e6ed
Enderlook <enderlook@gmail.com>
2020-09-16 10:47:13 -0300

    Extract PlayerSpawn from ThirdPersonMovementRigidbody

---
+++ Assets/Scripts/Player/PlayerSpawn.cs
@@ -0,0 +1,31 @@
+﻿using Avoidance.Scene;
+
+using Enderlook.Enumerables;
+using Enderlook.Unity.Navigation;
+using Enderlook.Unity.Navigation.D2;
+
+using UnityEngine;
+
+namespace Avoidance.Player
+{
+    [DefaultExecutionOrder(1)]
+    public class PlayerSpawn : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // Spawn Character with position and rotation
+            Node node = ((IGraphAtoms<Node, Edge>)MazeGenerator.Graph).Nodes.RandomPick();
+            Vector3 position = node.Position;
+            transform.position = new Vector3(position.x, 0, position.y);
+
+            // TODO: This rotation doesn't work fine...
+            Vector3 oldRotation = transform.rotation.eulerAngles;
+            Vector2 to = node.Edges.RandomPick().To.Position;
+            transform.LookAt(new Vector3(to.x, .5f, to.y));
+            Vector3 eulerAngles = transform.rotation.eulerAngles;
+            transform.rotation = Quaternion.Euler(new Vector3(oldRotation.x, eulerAngles.y, oldRotation.z));
+
+            Destroy(this);
+        }
+    }
+}
--- Assets/Scripts/Player/ThirdPersonMovementRigidbody.cs
+++ Assets/Scripts/Player/ThirdPersonMovementRigidbody.cs
@@ -86,18 +86,6 @@ namespace Avoidance.Player
                     freeLook = pivot.parent.GetComponent<FreeLookCamera>();
             }
 
-            // Spawn Character with position and rotation
-            Node node = ((IGraphAtoms<Node, Edge>)MazeGenerator.Graph).Nodes.RandomPick();
-            Vector3 position = node.Position;
-            transform.position = new Vector3(position.x, 0, position.y);
-
-            // TODO: This rotation doesn't work fine...
-            Vector3 oldRotation = transform.rotation.eulerAngles;
-            Vector2 to = node.Edges.RandomPick().To.Position;
-            transform.LookAt(new Vector3(to.x, .5f, to.y));
-            Vector3 eulerAngles = transform.rotation.eulerAngles;
-            transform.rotation = Quaternion.Euler(new Vector3(oldRotation.x, eulerAngles.y, oldRotation.z));
-
             stateMachine = StateMachine<PlayerState, PlayerEvent>.Builder()
                 .SetInitialState(PlayerState.Idle)
                 .In(PlayerState.Idle)
