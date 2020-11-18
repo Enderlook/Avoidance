c5b0d9d91664c29d50171dbd71c911f79498fb75
Braian Alejandro Ledesma <braian.ledesma-99@hotmail.com>
2020-11-18 06:47:50 -0300

    Fix bug of character not moving when the level start after win or lose

--- Assets/Scripts/Player/PlayerBrain.cs
+++ Assets/Scripts/Player/PlayerBrain.cs
@@ -117,6 +117,7 @@ namespace Avoidance.Player
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void Awake()
         {
+            Win = Death = false;
             Stamina = maximumStamina;
             staminaBar.ManualUpdate(Stamina, maximumStamina);
 
